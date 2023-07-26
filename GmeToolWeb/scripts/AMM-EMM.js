94937db130e3805f26a3af582ab4fca3bebf50a6
Brian Hu <bhu1489@gmail.com>
2023-07-26 11:56:44 +0800

    GmeTool
    修改 S76 发动机定检 EMM 识别错误

--- GmeToolWeb/scripts/AMM-EMM.js
+++ GmeToolWeb/scripts/AMM-EMM.js
@@ -1,7 +1,7 @@
   //---AMM & EMM REV 在此处更改---------
 
-  const S92AMMRev = "REV.40";   //S92 - AMM 
-  const S92EMMRev = "REV.20";   //S92 - EMM
+  const S92AMMRev = "REV.41";   //S92 - AMM 
+  const S92EMMRev = "REV.21";   //S92 - EMM
 
 
   const S76CAMMRev = "REV.33";  //S76 - AMM
--- GmeToolWeb/scripts/S92Tool.js
+++ GmeToolWeb/scripts/S92Tool.js
@@ -6,10 +6,12 @@
   let workerTempObj =[];  //用于存储暂存列表的临时数组
 
   //---机型选择下拉框的临时变量------
-  let aircraftSelected = document.getElementById("craftModel");
+  const aircraftSelected = document.getElementById("craftModel");
+
   let aircraftSelectedResult = aircraftSelected.options[aircraftSelected.selectedIndex].value;
 
-  //--------获取并显示当前日期-------
+
+  //--------获取并显示当前日期-----------
   let ddd = new Date();
   let day = ddd.getDate();
   let month = ddd.getMonth() + 1;
@@ -22,28 +24,32 @@
   let datew = ddd.getFullYear() + "-" + month + "-" + day;
   datew = datew.toString();
   //console.log(datew);
-  document.querySelector("#dateSelect").value = datew;
+  document.getElementById("dateSelect").value = datew;
+
+
 
   //----------设置默认机型为S92时-项目列表-AMMRev-EMMRev--------
   let writeOutput="";
   if(aircraftSelectedResult == "S76C"){
-    S76CCraftProject.forEach((str)=>{
-      writeOutput += "<option value=" + "\"" + str + "\"" + ">" + str + "</option>";
-    })
+    for(let key in chapterS76C){
+      writeOutput += `<option value=${key.replace(/\s+/g, '-')}>${key}</option>`;
+    }
     document.getElementById("AMM").value = S76CAMMRev;
     document.getElementById("EMM").value = S76CEMMRev;
   }else if(aircraftSelectedResult == "S92"){
-    S92CraftProject.forEach((str)=>{
-      writeOutput += "<option value="+ str + ">" + str + "</option>";
-    })
+    for(let key in chapterS92){
+      writeOutput += `<option value=${key.replace(/\s+/g, '-')}>${key}</option>`;
+    }
     document.getElementById("AMM").value = S92AMMRev;
     document.getElementById("EMM").value = S92EMMRev;
   }
   document.getElementById("selectProject").innerHTML = writeOutput;
 
+
   //------------按下按钮复制-------
-  var buttonCopy = document.getElementById('buttonCopy');
-  var text = document.getElementById('textYj');
+  let buttonCopy = document.getElementById('buttonCopy');
+  let text = document.getElementById('textYj');
+
   buttonCopy.addEventListener('click', function(){
     text.focus(); // 文本框获取焦点事件，以便于获取选中位置
     text.setSelectionRange(0, -1); // 设置选中范围，只有文本框有此事件
@@ -51,9 +57,8 @@
   })
 
   //--------------生成YJ-----------------
-  var buttonGenerate = document.getElementById('buttonGenerate');
-  buttonGenerate.addEventListener('click', function(){
-    aircraftSelected = document.getElementById("craftModel");
+  document.getElementById('buttonGenerate').addEventListener('click', function(){
+    // aircraftSelected = document.getElementById("craftModel");
     aircraftSelectedResult = aircraftSelected.options[aircraftSelected.selectedIndex].value;
     if(!document.getElementById("FuselageTime").value){
       alert("请输入飞行时间！");
@@ -62,12 +67,12 @@
     let myProjectSelectIndex = myProject.selectedIndex;
     let textYJ;
     let MM;
-    if( myProject.options[myProjectSelectIndex].value.search("发动机") != -1 )
+    if( myProject.options[myProjectSelectIndex].innerHTML.search("发动机") != -1 )
     {
       MM="EMM";
     }else{MM="AMM";}
     if(aircraftSelectedResult == "S92"){
-      textYJ = "依据民航法规及" + MM + "(" + document.getElementById(MM=="EMM"?"EMM":"AMM").value + ")" + "第" + chapterS92[myProject.options[myProjectSelectIndex].value] + "章节等相关内容" + "，" + 
+      textYJ = "依据民航法规及" + MM + "(" + document.getElementById(MM=="EMM"?"EMM":"AMM").value + ")" + "第" + chapterS92[myProject.options[myProjectSelectIndex].innerHTML] + "章节等相关内容" + "，" + 
         "于" + document.getElementById("dateSelect").value.substr(0,4) + "年" 
         + document.getElementById("dateSelect").value.substr(5,2) + "月" 
         + document.getElementById("dateSelect").value.substr(8,2) + "日,"
@@ -75,7 +80,7 @@
         + myProject.options[myProjectSelectIndex].innerHTML + "定检，检查结果正常，限于本项工作，飞机适航。";
         console.log(myProject.options[myProjectSelectIndex].value);
     }else if(aircraftSelectedResult == "S76C"){
-      textYJ = "依据民航法规及" + MM + "(" + document.getElementById(MM=="EMM"?"EMM":"AMM").value + ")" + "第" + chapterS76C[myProject.options[myProjectSelectIndex].value] + "章节等相关内容" + "，" + 
+      textYJ = "依据民航法规及" + MM + "(" + document.getElementById(MM=="EMM"?"EMM":"AMM").value + ")" + "第" + chapterS76C[myProject.options[myProjectSelectIndex].innerHTML] + "章节等相关内容" + "，" + 
         "于" + document.getElementById("dateSelect").value.substr(0,4) + "年" 
         + document.getElementById("dateSelect").value.substr(5,2) + "月" 
         + document.getElementById("dateSelect").value.substr(8,2) + "日,"
@@ -90,18 +95,20 @@
   //----------更改机型回调--------------
   function changeAircraft(){
     writeOutput = "";
-    aircraftSelected = document.getElementById("craftModel");
+    // aircraftSelected = document.getElementById("craftModel");
     aircraftSelectedResult = aircraftSelected.options[aircraftSelected.selectedIndex].value;
     if(aircraftSelectedResult == "S76C"){
-      S76CCraftProject.forEach((str)=>{
-        writeOutput += "<option value=" + "\"" + str + "\"" + ">" + str + "</option>";
-      })
+
+      for(let key in chapterS76C){
+        writeOutput += `<option value=${key.replace(/\s+/g, '-')}>${key}</option>`;
+      }
       document.getElementById("AMM").value = S76CAMMRev;
       document.getElementById("EMM").value = S76CEMMRev;
     }else if(aircraftSelectedResult == "S92"){
-      S92CraftProject.forEach((str)=>{
-        writeOutput += "<option value="+ str + ">" + str + "</option>";
-      })
+
+      for(let key in chapterS92){
+        writeOutput += `<option value=${key.replace(/\s+/g, '-')}>${key}</option>`;
+      }
       document.getElementById("AMM").value = S92AMMRev;
       document.getElementById("EMM").value = S92EMMRev;
     }
@@ -120,7 +127,7 @@
 
 
     //----------show the data whitch matches -------
-    var str = "";
+    let str = "";
 
 
     //-----采用for循环写表格-----
@@ -140,13 +147,12 @@
 
 
     //-------------------增加鼠标悬停列表颜色改变------------
-    var trTable = document.getElementsByTagName("tr");
-    for(var i= 0;i<trTable.length;i++){
+    let trTable = document.getElementsByTagName("tr");
+    for(let i= 0;i<trTable.length;i++){
       bgcChange(trTable[i]);
     }
   }
     
-
 
 
   //----------点击搜索后得到的列表数据------将员工号添加到上部Sticky列表暂存------------
@@ -158,23 +164,15 @@
       // let obj = {"name":name,"code":code};
       let obj = {"num":stickyNum++,"workerName":name,"workerCode":code};
       workerTempObj.push(obj);//往临时数组内暂存当前点击的对象，用以检索是否重复显示在暂存列表
-      // let strSticky="";
-      // strSticky += `<tr id=${"sticky"+name} ondblclick='stickyOndblclick("${name}","${code}")'>`
-      // + `<td>${stickyNum++}</td>` 
-      // + `<td>${name}</td>` 
-      // + `<td>${code}</td>` 
-      // + "</tr>";
-      // document.getElementById("stickyTb").innerHTML += strSticky;  //在<tbody>标签内新增行
       writeTableFromJsonObj(workerTempObj,"stickyTb");
       if(stickyNum>10){
         alert("StickyFull");
-      }
-      
+      }  
     }
 
     //增加鼠标悬停列表颜色改变
-    var trTable = document.getElementsByTagName("tr");
-    for(var i= 0;i<trTable.length;i++){
+    let trTable = document.getElementsByTagName("tr");
+    for(let i= 0;i<trTable.length;i++){
       bgcChange(trTable[i]);
     }
     writeLog("添加到Sticky列表");
@@ -224,16 +222,6 @@
   //-----------依据--Json对象-----写表格函数-----------------
 function writeTableFromJsonObj(jsonData , tbId){   //jsonData是一个JSON{}对象,tb是表格的ID
   let tbString = "";
-  // for(let key in jsonData){
-  //     // console.log(key + " " + jsonData[key].fuselageTime);
-  //     // tbString += `<tr id=${key} contenteditable="true">` 
-  //     tbString +=  `<tr  ondblclick='stickyOndblclick("${jsonData[key].workerName}","${jsonData[key].workerCode}")'>` //这个位置已经取消表格可编辑
-  //     + '<td>'+ key +'</td>' 
-  //     + '<td>' + jsonData[key].workerName + '</td>' 
-  //     + '<td>'+ jsonData[key].workerCode + '</td>' 
-  //     // + '<td>'+ jsonData[key].engineTwoTime + '</td>' 
-  //     + '</tr>';
-  // }
   jsonData.forEach((obj)=>{
         tbString +=  `<tr  ondblclick='stickyOndblclick("${obj.workerName}","${obj.workerCode}")'>` //这个位置已经取消表格可编辑
         + '<td>'+ obj.num +'</td>' 
@@ -260,25 +248,9 @@ function readTableToJson(tbID){
     let tdValue;
     let rows = document.getElementById(tbID).rows.length;
     // let colums = document.getElementById("tb").rows[0].cells.length;
-
-    for (var i = 0; i < rows; i++) { //每行
-        // let craftIDTemp = document.getElementById(tbID).rows[i].cells[0].innerHTML;
-        // if(craftIDTemp === "" && i === 0 && rows === 1){//如果在读取第一行时飞机号列表栏为空，跳过当前行数据读取
-        //     json += "{";
-        //     continue;
-        // }else if(craftIDTemp === "" && i === 0 && rows !== 1){//如果在读取第一行时飞机号列表栏为空，跳过当前行数据读取
-          
-        //     continue;
-        // }else if(craftIDTemp === "" && i !== 0){//不是第一行飞机号列为空时
-        //     continue;
-        // }
-
-        // json += "\""; //加上一个双引号
-        // json += document.getElementById(tbID).rows[i].cells[0].innerHTML;
-        // json += "\""; 
-        // json += ":";
+    for (let i = 0; i < rows; i++) { //每行
         json += "{";
-        for (var j = 0; j < 3; j++) {
+        for (let j = 0; j < 3; j++) {
             //tdName = KeysArray[j]; //Json数据的键
             tdValue = document.getElementById(tbID).rows[i].cells[j].innerHTML;//Json数据的值
             json += "\""; //加上一个双引号
@@ -306,8 +278,7 @@ document.getElementById("saveList").addEventListener("click",()=>{
   // if(document.getElementById("stickyTb").innerHTML){
   let saveData = readTableToJson("stickyTb");
   window.localStorage.setItem("WorkListObj",saveData);
-
-  
+ 
 });
 
 window.onload = function(){
